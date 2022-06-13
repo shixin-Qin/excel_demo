310d5c47130499f3a4e0e40848037aab5bc04942
qinshixin <qinshixin@fosunhealth.com>
2022-06-13 21:33:32 +0800

    提交

--- src/test/java/com/example/excel_demo/ExcelDemoApplicationTests.java
+++ src/test/java/com/example/excel_demo/ExcelDemoApplicationTests.java
@@ -207,4 +207,78 @@ class ExcelDemoApplicationTests {
         fileWriter3.close();
         
     }
+    
+    @Test
+    void refreshProd() throws IOException {
+        // 1.1 读取总的list
+        String result_path = "D:\\项目文档\\20220428\\sku\\清洗数据\\prod\\prod_result.xlsx";
+        Sheet sheet = new Sheet(1, 1, SkuSuppilerMode.class);
+        
+        // 这里 取出来的是 ExcelModel实体 的集合
+        List<Object> readList = EasyExcelFactory.read(new FileInputStream(result_path), sheet);
+        // 存 ExcelMode 实体的 集合
+        HashMap<String, Integer> resultMap = new HashMap<>();
+        for (Object obj : readList) {
+            SkuSuppilerMode skuSuppilerMode = (SkuSuppilerMode) obj;
+            Integer supplierId = skuSuppilerMode.getSupplierId();
+            if(Objects.nonNull(supplierId) && !supplierId.equals(0)){
+                resultMap.put(skuSuppilerMode.getSkuId(),skuSuppilerMode.getSupplierId());
+            }
+        }
+        
+        // 2.1.1 读取platform_scenter_product
+        String readPath1 = "D:\\项目文档\\20220428\\sku\\清洗数据\\prod\\platform_scenter_product.xlsx";
+        Sheet sheet1 = new Sheet(1, 1, SkuSuppilerMode.class);
+        List<Object> readList1 = EasyExcelFactory.read(new FileInputStream(readPath1), sheet1);
+        // 2.1.2 生成sql
+        BufferedWriter fileWriter1 = Files.newBufferedWriter(Paths.get("D:\\项目文档\\20220428\\sku\\清洗数据\\prod\\platform_scenter_product.sql"));
+        String format1 = "UPDATE platform_scenter_product SET supplier_id = %s WHERE sku_id = %s;";
+        for (Object obj : readList1) {
+            SkuSuppilerMode one = (SkuSuppilerMode) obj;
+            Integer supplierId = resultMap.get(one.getSkuId());
+            if(Objects.nonNull(supplierId) && !supplierId.equals(0)){
+                String updateSql = String.format(format1,supplierId,one.getSkuId());
+                fileWriter1.write(updateSql);
+                fileWriter1.newLine();
+            }
+        }
+        fileWriter1.close();
+        
+        // 2.2.1 读取doc_drug_collect_record
+        String readPath2 = "D:\\项目文档\\20220428\\sku\\清洗数据\\prod\\doc_drug_collect_record.xlsx";
+        Sheet sheet2 = new Sheet(1, 1, SkuSuppilerMode.class);
+        List<Object> readList2 = EasyExcelFactory.read(new FileInputStream(readPath2), sheet2);
+        // 2.2.2 生成sql
+        BufferedWriter fileWriter2 = Files.newBufferedWriter(Paths.get("D:\\项目文档\\20220428\\sku\\清洗数据\\prod\\doc_drug_collect_record.sql"));
+        String format2 = "UPDATE doc_drug_collect_record SET supplier_id = %s WHERE drug_id = %s;";
+        for (Object obj : readList2) {
+            SkuSuppilerMode one = (SkuSuppilerMode) obj;
+            Integer supplierId = resultMap.get(one.getSkuId());
+            if(Objects.nonNull(supplierId) && !supplierId.equals(0)){
+                String updateSql = String.format(format2,supplierId,one.getSkuId());
+                fileWriter2.write(updateSql);
+                fileWriter2.newLine();
+            }
+        }
+        fileWriter2.close();
+        
+        // 2.3 读取doctor_prescribe_medicine_rel
+        String readPath3 = "D:\\项目文档\\20220428\\sku\\清洗数据\\prod\\doctor_prescribe_medicine_rel.xlsx";
+        Sheet sheet3 = new Sheet(1, 1, SkuSuppilerMode.class);
+        List<Object> readList3 = EasyExcelFactory.read(new FileInputStream(readPath3), sheet3);
+        // 2.2.2 生成sql
+        BufferedWriter fileWriter3 = Files.newBufferedWriter(Paths.get("D:\\项目文档\\20220428\\sku\\清洗数据\\prod\\doctor_prescribe_medicine_rel.sql"));
+        String format3 = "UPDATE doctor_prescribe_medicine_rel SET supplier_id = %s WHERE sku_id = \"%s\";";
+        for (Object obj : readList3) {
+            SkuSuppilerMode one = (SkuSuppilerMode) obj;
+            Integer supplierId = resultMap.get(one.getSkuId());
+            if(Objects.nonNull(supplierId) && !supplierId.equals(0)){
+                String updateSql = String.format(format3,supplierId,one.getSkuId());
+                fileWriter3.write(updateSql);
+                fileWriter3.newLine();
+            }
+        }
+        fileWriter3.close();
+        
+    }
 }
